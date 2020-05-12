3cea59bb15c0c5c5988d9c95bee399d561b0f102
Karthick Kumar V <karthick.velraj@fpsinc.com>
2020-05-12 15:24:40 +0530

    get value from input tag

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class AppComponent extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  constructor(props) {
+    super(props);
+    this.state = {
+      usernameData: '',
+      passwordData: ''
+    }
+  }
+  onLogin() {
+    console.log(this.state)
+  }
+
+  handleInput(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Login Form</h2>
+        <label>Enter Email id: </label>
+        <input type="email" name="usernameData" onChange={this.handleInput.bind(this)}></input>
+        <span>{this.state.usernameData}</span>
+        <br></br>
+        <label>Enter Password: </label>
+        <input type="password" name="passwordData" onChange={this.handleInput.bind(this)}></input>
+        <span>{this.state.passwordData}</span>
+        <br></br>
+        <button onClick={() => this.onLogin()}>Login</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<AppComponent />, document.getElementById('root'));
